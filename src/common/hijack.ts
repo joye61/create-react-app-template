e97eb5bf70a2a7ba55f71de57549645e70df81ff
joye <zhoujing@chelun.com>
2020-02-24 14:34:24 +0800

    update hijack

---
+++ src/common/hijack.ts
@@ -0,0 +1,79 @@
+/**
+ * 配合simple-http-proxy使用
+ *
+ * @param targetUrl
+ * @param proxyParam
+ * @param proxyOrigin
+ */
+
+/**
+ *
+ * @param targetUrl
+ * @param proxyParam
+ * @param proxyOrigin
+ */
+export function hijack(
+  targetUrl: string,
+  proxyParam: string = "__proxy",
+  proxyOrigin: string = "http://localhost:4000"
+) {
+  try {
+    const proxy = new URL(proxyOrigin);
+    proxy.searchParams.set(proxyParam, targetUrl);
+    return proxy.href;
+  } catch (error) {}
+
+  return targetUrl;
+}
+
+export function hijackGlobally(
+  proxyParam: string = "__proxy",
+  proxyOrigin: string = "http://localhost:4000"
+) {
+  // 拦截window.XMLHTTPRequest
+  const rawSend = window.XMLHttpRequest.prototype.open;
+  window.XMLHttpRequest.prototype.open = function(
+    method: string,
+    url: string,
+    async: boolean = true,
+    username?: string | null,
+    password?: string | null
+  ) {
+    const proxyUrl = hijack(url, proxyParam, proxyOrigin);
+    rawSend.call(this, method, proxyUrl, async, username, password);
+  };
+
+  // 拦截window.fetch
+  if (typeof window.fetch === "function") {
+    const rawFetch = window.fetch;
+    window.fetch = function(
+      input: RequestInfo,
+      init?: RequestInit | undefined
+    ): Promise<Response> {
+      if (typeof input === "string") {
+        const proxyUrl = hijack(input, proxyParam, proxyOrigin);
+        return rawFetch(proxyUrl, init);
+      }
+
+      if (input instanceof Request) {
+        const proxyUrl = hijack(input.url, proxyParam, proxyOrigin);
+        return rawFetch(
+          new Request(proxyUrl, {
+            method: input.method,
+            headers: input.headers,
+            body: input.body,
+            mode: input.mode,
+            credentials: input.credentials,
+            cache: input.cache,
+            redirect: input.redirect,
+            referrer: input.referrer,
+            integrity: input.integrity
+          }),
+          init
+        );
+      }
+
+      return rawFetch(input, init);
+    };
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
 import "./index.scss";
-import { Loader } from "./common/loader";
+// import { Loader } from "./common/loader";
+
+/**
+ * 是否启用代理功能
+ */
+if (process.env.NODE_ENV === "development") {
+  import("./common/hijack").then(result => {
+    result.hijackGlobally();
+  });
+}
 
 // 启动App的执行
-async function RunApp(){
+async function RunApp() {
   const app = await import("./app");
   app.Run();
 }
 
 // 如果需要预先展示加载百分比执行此处逻辑
-const loader = new Loader();
-loader.onProgress(percent => {
-  console.log(percent);
-});
-loader.onEnd(() => {
-  console.log("end");
-  RunApp();
-});
-
+// const loader = new Loader();
+// loader.onProgress(percent => {
+//   // console.log(percent);
+// });
+// loader.onEnd(() => {
+//   // console.log("end");
+//   RunApp();
+// });
 
 // 如果不需要展示百分比，直接加载
-// RunApp();
+RunApp();
